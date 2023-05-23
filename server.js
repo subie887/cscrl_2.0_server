b526bd3e54d2ee99d149f38411fd62fb98838d85
Illia Slipchenko <51210392+subie887@users.noreply.github.com>
2023-05-23 16:45:59 -0400

    headers are back

--- server.js
+++ server.js
@@ -35,14 +35,14 @@ app.use(
 
 app.options('*', cors());
 
-/*
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
-*/
+
 
 const prisma = new PrismaClient()
 
